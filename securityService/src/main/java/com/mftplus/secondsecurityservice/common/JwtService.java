fba75115a44d445fea15fbeea017a06d7a2fd8ca
mahursh <127805335+mahursh@users.noreply.github.com>
2024-05-22 02:08:52 +0430

    Jwt Service And Filter Added

---
+++ securityService/src/main/java/com/mftplus/secondsecurityservice/common/JwtService.java
@@ -0,0 +1,45 @@
+package com.mftplus.secondsecurityservice.common;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtService {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+    @Value("${jwt.issuer}")
+
+    private String issuer; public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setIssuer(issuer)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+    public String validateToken(final String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ securityService/src/main/java/com/mftplus/secondsecurityservice/filter/JwtTokenFilter.java
@@ -0,0 +1,11 @@
+package com.mftplus.secondsecurityservice.filter;
+
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtTokenFilter{
+
+
+}
