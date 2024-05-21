534e6e56e68afe6d6b976f3ec4eb894c0a5a18ee
mahursh <127805335+mahursh@users.noreply.github.com>
2024-05-22 02:09:31 +0430

    UserDetailsInfo Added

---
+++ securityService/src/main/java/com/mftplus/secondsecurityservice/dto/UserDetailsInfo.java
@@ -0,0 +1,59 @@
+package com.mftplus.secondsecurityservice.dto;
+
+import com.mftplus.secondsecurityservice.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserDetailsInfo implements UserDetails {
+    private String name;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public UserDetailsInfo(User userInfo) {
+        name=userInfo.getUsername();
+        password=userInfo.getPassword();
+        authorities= Arrays.stream(userInfo.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
