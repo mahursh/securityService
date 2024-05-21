3d414eb435b9bf3326acd5bd756fe13ca78127e4
Mahoor Shams <127805335+mahursh@users.noreply.github.com>
2024-05-21 21:19:10 +0330

    first init

---
+++ securityService/src/main/java/com/mftplus/secondsecurityservice/SecondSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mftplus.secondsecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSecurityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSecurityServiceApplication.class, args);
+    }
+
+}
---
+++ securityService/src/test/java/com/mftplus/secondsecurityservice/SecondSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mftplus.secondsecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSecurityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
